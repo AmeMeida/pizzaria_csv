ba3f913ff2b07b048c3beb5bb808b39b688694ff
AmeMeida <ame.pistache@gmail.com>
2023-11-22 23:51:48 -0300

    arrumando o link por motivos de simone

--- partials/head.php
+++ partials/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <base href="https://c84e-2804-868-d041-5517-a940-ec62-b861-2dea.ngrok-free.app/csv/ ">
+    <base href="http://localhost/pizzaria_csv-main/public/">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
     <script src="https://unpkg.com/htmx.org@1.9.9" integrity="sha384-QFjmbokDn2DjBjq+fM+8LUIVrAgqcNW2s0PjAxHETgRn9l4fvX31ZxDxvwQnyMOX" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./style/reset.css">
